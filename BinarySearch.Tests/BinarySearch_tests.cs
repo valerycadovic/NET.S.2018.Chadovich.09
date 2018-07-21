3fbb10d635839371d9f38720a6c6ffd031fa1a8f
Valery Čadovič <valerachad03@gmail.com>
2018-07-21 14:47:11 +0300

    Binary search added

---
+++ BinarySearch.Tests/BinarySearch_tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinarySearchLib;
+using NUnit.Framework;
+
+namespace BinarySearch_Tests
+{
+    [TestFixture]
+    public class BinarySearch_Tests
+    {
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 2, ExpectedResult = 1)]
+        [TestCase(new int[] { 1, 1, 1, 4, 5 }, 1, ExpectedResult = 0)]
+        [TestCase(new int[] { 1, 1, 1, 4, 5, 6 }, 1, ExpectedResult = 0)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 5 }, 5, ExpectedResult = 4)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 6, ExpectedResult = -1)]
+        [TestCase(new int[] { 1, 2, 3, 4, 7 }, 6, ExpectedResult = -1)]
+        [TestCase(new int[] {}, 2, ExpectedResult = -1)]
+        public int Can_Search_On_Int(int[] array, int item)
+        {
+            int compare(int a, int b)
+            {
+                if (a > b) return 1;
+                if (a < b) return -1;
+                return 0;
+            }
+
+            return BinarySearch<int>.Search(array, item, compare);
+        }
+
+        [Test]
+        public void Throws_ArgumentNull_On_Null_Array()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                BinarySearch<int>.Search(null, 1, Comparer<int>.Default.Compare));
+        }
+
+        [Test]
+        public void Throws_ArgumentNull_On_Null_Comparison()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                BinarySearch<int>.Search(new int[]{}, 1, null));
+        }
+
+        [TestCase(1, 6)]
+        [TestCase(-1, 3)]
+        [TestCase(4, 3)]
+        [TestCase(-2, -1)]
+        [TestCase(7, 8)]
+        [TestCase(3, 3)]
+        public void Throws_When_Invalid_Borders(int start, int end)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                BinarySearch<int>.Search(new int[] {1, 2, 3, 4, 5}, 1, Comparer<int>.Default.Compare, start, end));
+        }
+    }
+}
---
+++ BinarySearchLib/BinarySearch.cs
@@ -0,0 +1,135 @@
+﻿namespace BinarySearchLib
+{
+    using System;
+
+    /// <summary>
+    /// Binary search class
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type of sorting elements
+    /// </typeparam>
+    public static class BinarySearch<T>
+    {
+        #region Public API
+        /// <summary>
+        /// Searches the element in sorted array.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="item">The item.</param>
+        /// <param name="compare">The compare.</param>
+        /// <returns>
+        /// element which was found
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Throws when array or compare is null
+        /// </exception>
+        public static int Search(T[] array, T item, Comparison<T> compare)
+            => Search(array, item, compare, 0);
+
+        /// <summary>
+        /// Searches the element in sorted array.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="item">The item.</param>
+        /// <param name="compare">The compare.</param>
+        /// <param name="start">The start.</param>
+        /// <returns>
+        /// element which was found
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Throws when array or compare is null
+        /// </exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">invalid borders</exception>
+        public static int Search(T[] array, T item, Comparison<T> compare, int start)
+        {
+            ValidateOnNull(array, nameof(array));
+
+            return Search(array, item, compare, start, array.Length);
+        }
+
+        /// <summary>
+        /// Searches the element in sorted array.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="item">The item.</param>
+        /// <param name="compare">The compare.</param>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <returns>
+        /// element which was found
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Throws when array or compare is null
+        /// </exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">invalid borders</exception>
+        public static int Search(T[] array, T item, Comparison<T> compare, int start, int end)
+        {
+            ValidateOnNull(array, nameof(array));
+            ValidateOnNull(compare, nameof(compare));
+            ValidateBorders(start, end, array.Length);
+
+            if (array.Length == 0 || compare(item, array[start]) < 0 || compare(item, array[end - 1]) > 0)
+            {
+                return -1;
+            }
+
+            while (end > start)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (compare(item, array[mid]) <= 0)
+                {
+                    end = mid;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            if (compare(array[end], item) == 0)
+            {
+                return end;
+            }
+
+            return -1;
+        }
+        #endregion
+
+        #region Private Validation methods
+        /// <summary>
+        /// Validates the on null.
+        /// </summary>
+        /// <typeparam name="V">
+        /// Type of validating parameter
+        /// </typeparam>
+        /// <param name="obj">The object.</param>
+        /// <param name="name">The name.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Throws when object is null
+        /// </exception>
+        private static void ValidateOnNull<V>(V obj, string name) where V : class
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException($"{name} is null");
+            }
+        }
+
+        /// <summary>
+        /// Validates the borders.
+        /// </summary>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <param name="length">The length.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">invalid borders</exception>
+        private static void ValidateBorders(int start, int end, int length)
+        {
+            if (start >= end || start < 0 || start >= length || end < 0 || end > length)
+            {
+                throw new ArgumentOutOfRangeException("invalid borders");
+            }
+        }
+        #endregion
+    }
+}

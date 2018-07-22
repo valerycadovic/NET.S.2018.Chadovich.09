e12daf1d7461dba75eb46d43decf2cd4ee6b5153
Valery Čadovič <valerachad03@gmail.com>
2018-07-22 15:26:27 +0300

    long removed

--- FibonacciGenerator.Tests/FibonacciGenerator_Tests.cs
+++ FibonacciGenerator.Tests/FibonacciGenerator_Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using FibonacciLib;
 using NUnit.Framework;
@@ -9,20 +10,34 @@ namespace FibonacciGenerator.Tests
     [TestFixture]
     public class FibonacciGenerator_Tests
     {
-        [TestCase(5, new int[] {0, 1, 1, 2, 3})]
-        [TestCase(1, new int[] {0})]
-        [TestCase(2, new int[] {0, 1})]
-        [TestCase(10, new int[] {0, 1, 1, 2, 3, 5, 8, 13, 21, 34})]
-        public void Can_Generate_Fibonacci(int n, int[] expected)
+        [Test]
+        public void Can_Generate_BigInteger_Fibonacci()
         {
-            IEnumerable<long> result = Fibonacci.Generate(n);
-            CollectionAssert.AreEqual(expected, result);
+            const int assertions = 4;
+
+            BigInteger[][] expected =
+            {
+                new BigInteger[]{0, 1, 1, 2, 3},
+                new BigInteger[]{0},
+                new BigInteger[]{0, 1},
+                new BigInteger[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}
+            };
+
+            for (int i = 0; i < assertions; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], Fibonacci.GenerateBigInteger(expected[i].Length));
+            }
         }
+
+        [TestCase(500, ExpectedResult = 500)]
+        [TestCase(1000, ExpectedResult = 1000)]
+        [TestCase(10000, ExpectedResult = 10000)]
+        public int Can_Generate_Big_Count_Of_Fibonaccies(int n)
+            => Fibonacci.GenerateBigInteger(n).ToArray().Length;
 
         [TestCase(-1)]
         [TestCase(0)]
-        [TestCase(93)]
         public void Throws_When_N_Is_Non_Positive(int n)
-            => Assert.Throws<ArgumentOutOfRangeException>(() => Fibonacci.Generate(n));
+            => Assert.Throws<ArgumentOutOfRangeException>(() => Fibonacci.GenerateBigInteger(n));
     }
 }
--- FibonacciLib/Fibonacci.cs
+++ FibonacciLib/Fibonacci.cs
@@ -9,41 +9,7 @@
     /// </summary>
     public static class Fibonacci
     {
-        #region Private constants
-        /// <summary>
-        /// The maximum long fibonacci index
-        /// </summary>
-        private const int MaxLongFibonacciIndex = 92;
-        #endregion
-
         #region Public API
-        /// <summary>
-        /// Generates a collection of n fibonacci numbers
-        /// </summary>
-        /// <param name="n">The n.</param>
-        /// <returns>The collection of n fibonacci numbers.</returns>
-        /// <exception cref="System.ArgumentOutOfRangeException">Throws when n is non-positive or </exception>
-        public static IEnumerable<long> Generate(int n)
-        {
-            if (n <= 0 || n > MaxLongFibonacciIndex)
-            {
-                throw  new ArgumentOutOfRangeException($"{nameof(n)} must be positive");
-            }
-
-            IEnumerable<long> Find()
-            {
-                (long a, long b) = (0, 1);
-
-                while (n-- > 0)
-                {
-                    yield return a;
-                    (a, b) = (b, a + b);
-                }
-            }
-
-            return Find();
-        }
-
         /// <summary>
         /// Generates a collection of n big integer fibonacci numbers
         /// </summary>
@@ -52,7 +18,7 @@
         /// <exception cref="System.ArgumentOutOfRangeException">n</exception>
         public static IEnumerable<BigInteger> GenerateBigInteger(int n)
         {
-            if (n <= 0 || n > MaxLongFibonacciIndex)
+            if (n <= 0)
             {
                 throw new ArgumentOutOfRangeException($"{nameof(n)} must be positive");
             }
